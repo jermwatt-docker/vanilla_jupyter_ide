a7809308f2b4e1279a488d16a8d18417793092b9
jermwatt <jermwatt@gmail.com>
2022-07-06 08:07:22 -0700

    compose starter added

---
+++ compose_jupyter.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo '                  **********                  '
+echo '                  **********                  '
+echo '*** JUPYTER_IDE: BEGINNING STARTUP PROCESSES ***'
+echo '                  **********                  '
+echo '                  **********                  '
+
+
+echo '---- STOPPING SERVICES ----'
+
+# spin down services
+docker-compose -f docker-compose-vanilla-jupyter.yml down 
+
+echo '---- STOPPING NETWORK ----'
+
+# spin down network
+docker network rm ide_network
+
+
+echo '---- SPINNING UP NEW NETWORK ----'
+
+# spin up new version of network
+docker network create --attachable --driver=bridge --subnet=10.2.0.0/24 ide_network
+sleep 5
+
+echo '---- GENERATING SERVICE IPs FOR COMPOSE FILES ----'
+rm .env
+touch .env
+
+#PUBLIC_IP=$(curl -s -4 icanhazip.com)
+JUPYTER_IP=10.2.0.2
+echo JUPYTER_IP=${JUPYTER_IP} > .env
+
+
+echo '---- STARTING SERVICES ----'
+docker-compose -f docker-compose-vanilla-jupyter.yml up -d
